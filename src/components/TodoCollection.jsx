34737be2bb6c6bbf41814a0e2a2956a077b706fc
travel15740 <travel15740@gmail.com>
2024-07-24 11:11:04 +0800

    feat: add props for todo item and collection, and put todos data by props

--- src/components/TodoCollection.jsx
+++ src/components/TodoCollection.jsx
@@ -1,14 +1,21 @@
 import TodoItem from './TodoItem';
 
-const TodoCollection = () => {
+const TodoCollection = ({
+  todos,
+  onToggleDone,
+  onSave,
+  onDelete,
+  onChangeMode,
+}) => {
   return (
     <div>
-      TodoCollection
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      {/* 拿到陣列資料後, 用迴圈取出及渲染 */}
+      {todos.map(todo => {
+        return (
+          <TodoItem key={todo.id} todo={todo} />
+        )
+      })}
+
     </div>
   );
 };
