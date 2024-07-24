38450977a61234f534944cd7bdaba963b5d9c954
travel15740 <travel15740@gmail.com>
2024-07-24 10:22:32 +0800

    設定路由 React Router

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,4 @@
-import ACLogoIcon  from './ac-logo.svg';
+import ACLogoIcon  from './ac-logo.svg?react';
 import CheckActiveIcon from './check-active.svg';
 import CheckCircleIcon from './check-circle.svg';
 import CheckHoverIcon from './check-hover.svg';
