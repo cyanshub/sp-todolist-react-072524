606fe8814fa25aa50df979a7c630acc26cc612c7
travel15740 <travel15740@gmail.com>
2024-07-25 17:54:47 +0800

    在 TodoPage 頁面串接 API 的瀏覽功能

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,32 +1,11 @@
 import { Footer, Header, TodoCollection, TodoInput } from '@/components';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { todoController } from '../apis/todo';
 
-const dummyTodos = [
-  {
-    title: 'Learn react-router',
-    isDone: true,
-    id: 1,
-  },
-  {
-    title: 'Learn to create custom hooks',
-    isDone: false,
-    id: 2,
-  },
-  {
-    title: 'Learn to use context',
-    isDone: true,
-    id: 3,
-  },
-  {
-    title: 'Learn to implement auth',
-    isDone: false,
-    id: 4,
-  },
-];
 
 const TodoPage = () => {
   const [inputValue, setInputValue] = useState('');
-  const [todos, setTodos] = useState(dummyTodos);
+  const [todos, setTodos] = useState([]);
 
   const handleChange = (value) => {
     setInputValue(value);
@@ -126,6 +105,24 @@ const TodoPage = () => {
       return preVTodos.filter((todo) => todo.id !== id);
     });
   };
+
+  // 使用 React Hook: useEffect 工具, 其可在每次畫面渲染時, 向後端拿資料
+  // 在串接 API 的情境中，往往是在「畫面渲染」已經完成之後才發送 API 拿資料
+  // 拿到 API 回傳的資料後, 使用 useState 的 set 方法替換資料
+  useEffect(() => {
+    // 使用 async 包裝 await 方法
+    const getTodosAsync = async () => {
+      try {
+        const todos = await todoController.getTodos(); // 拿到資料
+        // 替換資料
+        setTodos(todos.map((todo) => ({ ...todo, isEdit: false })));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    // 執行 async/await 方法
+    getTodosAsync();
+  }, []);
 
   return (
     <div>
