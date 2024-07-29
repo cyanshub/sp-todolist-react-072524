ff6dc3c3a3feb9bbf0a45731879e340be5b4cdf0
travel15740 <travel15740@gmail.com>
2024-07-29 14:13:40 +0800

    串接註冊功能: 建立串接設定檔之 register 方法

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -4,15 +4,18 @@ import axios from 'axios';
 const authURL = 'https://todo-list.alphacamp.io/api/auth';
 
 export const authController = {
-  login: async ({ username, password }) => {
+  login: async (payload) => {
     try {
+      // 從 payload 拿取資料
+      const { username, password } = payload;
+
       // 透過 axios 向後端發送請求, 並利用解構賦值拿取資料
       const { data } = await axios.post(`${authURL}/login`, {
         username,
         password,
       });
 
-      console.log("拿取後端回傳資料:", data)
+      console.log('拿取後端回傳資料:', data);
 
       // 若登入成功會取得 token, 回傳成功訊息, 並夾帶後端的資料
       const { authToken } = data;
@@ -23,6 +26,31 @@ export const authController = {
       return data;
     } catch (error) {
       console.error('[Login Failed]:', error);
+    }
+  },
+  register: async (payload) => {
+    try {
+      // 從 payload 拿取資料
+      const { username, email, password } = payload;
+
+      // 透過 axios 向後端發送請求, 並利用解構賦值拿取資料
+      const { data } = await axios.post(`${authURL}/register`, {
+        username,
+        email,
+        password,
+      });
+
+      console.log('拿取後端回傳資料:', data);
+
+      // 若登入成功會取得 token, 回傳成功訊息, 並夾帶後端的資料
+      const { authToken } = data;
+      if (authToken) {
+        return { success: true, ...data };
+      }
+
+      return data;
+    } catch (error) {
+      console.error('[Register Failed]:', error);
     }
   },
 };
