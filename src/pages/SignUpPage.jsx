cf6a40bbb411326f6f8c0ea39f422ec815b964f4
travel15740 <travel15740@gmail.com>
2024-07-29 14:50:21 +0800

    串接註冊功能: 在 SignUpPage 註冊頁面掛載功能

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -8,11 +8,53 @@ import { ACLogoIcon } from '@/assets/images';
 import { AuthInput } from '@/components';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { authController } from '../apis/auth';
+import Swal from 'sweetalert2';
 
 const SignUpPage = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  // 定義 onClick 觸發的 handler
+  const handleClick = async () => {
+    // 驗證輸入框的輸入
+    if (username.length === 0 || email.length === 0 || password.length === 0) {
+      return;
+    }
+
+    // 向後端發送請求, 執行登入功能, 把輸入資訊裝在 payload 裡面, 並拿到後端回傳資料
+    const { success, authToken } = await authController.register({
+      username,
+      email,
+      password,
+    });
+
+    // 設計登入成功時的行為: 呼叫 localStorage 方法, 儲存 authToken
+    if (success) {
+      localStorage.setItem('authToken', authToken);
+
+      // 成功提示訊息
+      Swal.fire({
+        title: '註冊成功!',
+        icon: 'success',
+        showConfirmButton: false,
+        timer: 1000,
+        position: 'top',
+      });
+
+      return;
+    }
+
+    // 失敗提示訊息
+    Swal.fire({
+      title: '註冊失敗!',
+      icon: 'error',
+      showConfirmButton: false,
+      timer: 1000,
+      position: 'top',
+    });
+  };
 
   return (
     <AuthContainer>
@@ -54,8 +96,8 @@ const SignUpPage = () => {
           }}
         />
       </AuthInputContainer>
-      <AuthButton>註冊</AuthButton>
-      
+      <AuthButton onClick={handleClick}>註冊</AuthButton>
+
       <Link to="/login">
         <AuthLinkText>取消</AuthLinkText>
       </Link>
