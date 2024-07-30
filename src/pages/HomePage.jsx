f7083bd51c4eede4e3c84ce8a4d1f15150970919
travel15740 <travel15740@gmail.com>
2024-07-30 18:32:25 +0800

    細節補完: 將HomePage改成轉址中繼站

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+import { useEffect } from 'react';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  // 掛載 AuthContext
+  const { isAuthenticated } = useAuth();
+
+  // 啟用轉址功能
+  const navigate = useNavigate();
+
+  // 使用 React Hook: useEffect 工具, 其可在每次畫面渲染時觸發
+  useEffect(() => {
+    // 用 isAuthenticated 判斷身分狀態，然後根據頁面需求轉向適當的位置
+    if (isAuthenticated) {
+      // 若有驗證則導向 TodoPage
+      navigate('/todos');
+    } else {
+      // 若沒驗證則導向 LoginPage
+      navigate('/login');
+    }
+  }, [navigate, isAuthenticated]);
 };
 
 export default HomePage;
